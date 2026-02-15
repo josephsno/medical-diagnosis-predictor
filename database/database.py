f68b3b27d56804a2c91272101b53efe1c56bb4b7
ARIRI JOSEPH ORITSEBEMIGHO <josephariri@afripointgroup.com>
2026-02-15 19:13:31 +0100

    added .env

--- database/database.py
+++ database/database.py
@@ -1,17 +1,14 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:password@localhost:5432/medical_db"
+load_dotenv()
 
-engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True  # helps avoid stale connections
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
